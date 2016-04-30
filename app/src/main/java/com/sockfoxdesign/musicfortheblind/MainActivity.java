e63c99f6642b113857cc616c00483777c59cc1cc
chrisjpozzi <rS4XFc6M>
2016-04-29 22:50:46 -0500

    Changed some colors and added some things.

--- app/src/main/java/com/sockfoxdesign/musicfortheblind/MainActivity.java
+++ app/src/main/java/com/sockfoxdesign/musicfortheblind/MainActivity.java
@@ -5,7 +5,6 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
